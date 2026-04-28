4c1d7e7d347462917d8f3e6f2b57b37708d57f51
Gustavo Fernandes de Almeida <gustty75rj@hotmail.com>
2026-04-28 15:38:35 -0300

    aula 4 navegacao

--- minha-aplicacao-angular/src/app/app.ts
+++ minha-aplicacao-angular/src/app/app.ts
@@ -1,14 +1,13 @@
 
-import { Component, signal } from '@angular/core';
-import { Aula3 } from './aula3/aula3';
+import { Component } from '@angular/core';
+//adicione o RouterOutlet no imports
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  standalone: true,
   selector: 'app-root',
-  imports: [Aula3],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css',
+  styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('controle-estoque');
-}
+export class App {}
+          
